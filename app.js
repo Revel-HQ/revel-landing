ccf3337c993d6a6f9aa745b9e79d4f208b80e498
danny235 <danielbarima235@gmail.com>
2022-07-14 17:38:23 +0100

    fixes to css index.html and subscribe

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 
+/* 
+a. Revel's use of short-form videos as the primary means of displaying skill makes it easier for both 
+white and blue collar job seekers to showcase what they can do. This also works for people 
+looking to recruit talent in either sector.
+
+Also
+
+b. Applying for any job on Revel is as easy asswiping right on the job post. This would instantly 
+send your portfolio to the employer for review.
+
+
+*/
+
+/*
+2.Revel is unique in the sense that it is equally effective for both white and blue collar jobs. So a makeup
+artist can look for lucrative job offers as easily as a HR manager on our app. Such homogeny 
+cannot be found on any other job platform out there.
+ */
+
+/* 3. Revel is currently in development and will be released on android and iOS platforms by the end of
+2022. If you want to get notified the moment we launch, please sign up and join our
+waiting list. */
+
+
+
+
 // video loop function
 window.addEventListener("load", videoScroll);
 window.addEventListener("scroll", videoScroll);
@@ -22,4 +48,20 @@ function videoScroll() {
       }
     }
   }
+}
+
+function showFaqs(i) {
+  var faqsItem = document.querySelectorAll(".faqsItem")
+  console.log(faqsItem[0].children[0].children[0])
+    var faqEl = faqsItem[i], 
+    svgPathEl = faqsItem[i].children[0].children[0];
+    if(faqEl.classList.contains("active")) {
+      faqEl.classList.remove("active");
+      if(svgPathEl.classList.contains("rotatePath")) {
+        svgPathEl.classList.remove("rotatePath");
+      }
+    } else {
+      faqEl.classList.add("active");
+      svgPathEl.classList.add("rotatePath");
+    }
 }
