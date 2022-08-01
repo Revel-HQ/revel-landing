069f6350c5590e84945168f3c15393781648e662
danny235 <danielbarima235@gmail.com>
2022-08-01 20:02:53 +0100

    fixed video and revel faq

--- app.js
+++ app.js
@@ -23,30 +23,7 @@ const swiper = new Swiper('.swiper', {
 
 });
 
-// video loop function
-// window.addEventListener("load", videoScroll);
-// window.addEventListener("scroll", videoScroll);
-// function videoScroll() {
-//   if (document.querySelectorAll("video").length > 0) {
-//     var windowHeight = window.innerHeight,
-//       videoEl = document.querySelectorAll("video");
 
-//     for (var i = 0; i < videoEl.length; i++) {
-//       var thisVideoEl = videoEl[i],
-//         videoHeight = thisVideoEl.clientHeight,
-//         videoClientRect = thisVideoEl.getBoundingClientRect().top;
-
-//       if (
-//         videoClientRect <= windowHeight - videoHeight * 0.5 &&
-//         videoClientRect >= 0 - videoHeight * 0.5
-//       ) {
-//         thisVideoEl.play();
-//       } else {
-//         thisVideoEl.pause();
-//       }
-//     }
-//   }
-// }
 var faqsItem = document.querySelectorAll(".faqsItem");
 
 faqsItem.forEach((faqItem, i) => {
