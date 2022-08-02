4bea61388cbea375ea235373172c55aa37c4969c
danny235 <danielbarima235@gmail.com>
2022-08-02 13:35:33 +0100

    added fixes for video heading and also added a smooth scroll when you click on a faq so the page will scroll to the answer

--- app.js
+++ app.js
@@ -24,6 +24,29 @@ const swiper = new Swiper('.swiper', {
 });
 
 
+window.smoothScroll = function(target) {
+  var scrollContainer = target;
+  do { //find scroll container
+      scrollContainer = scrollContainer.parentNode;
+      if (!scrollContainer) return;
+      scrollContainer.scrollTop += 1;
+  } while (scrollContainer.scrollTop == 0);
+
+  var targetY = 0;
+  do { //find the top of target relatively to the container
+      if (target == scrollContainer) break;
+      targetY += target.offsetTop;
+  } while (target = target.offsetParent);
+
+  scroll = function(c, a, b, i) {
+      i++; if (i > 30) return;
+      c.scrollTop = a + (b - a) / 30 * i;
+      setTimeout(function(){ scroll(c, a, b, i); }, 20);
+  }
+  // start scrolling
+  scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
+
 var faqsItem = document.querySelectorAll(".faqsItem");
 
 faqsItem.forEach((faqItem, i) => {
@@ -41,6 +64,7 @@ faqsItem.forEach((faqItem, i) => {
     faqItem.classList.add("active");
     path.classList.add("rotatePath");
     showFaqs(faqsItem);
+    smoothScroll(document.getElementById('faqsContainer'))
 
     // remove active class from active element on second click
     if (faqsItemActive === null) return;
