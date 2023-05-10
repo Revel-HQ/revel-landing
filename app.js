be862d92fb868d2a59dc8aaf656c4b067da25a64
danny235 <danielbarima235@gmail.com>
2023-05-10 21:26:15 +0100

    second new changes and edits

--- app.js
+++ app.js
@@ -28,121 +28,122 @@ document.onreadystatechange = function () {
 };
 
 document.querySelector("#nav-barx").style.display = "none";
-    document.onreadystatechange = function () {
-      document.querySelector("body").style.visibility = "hidden";
-      if (document.readyState !== "complete") {
-        document.querySelector("body").style.visibility = "hidden";
+document.onreadystatechange = function () {
+  document.querySelector("body").style.visibility = "hidden";
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
 
-        document.querySelector("#loader").style.visibility = "visible";
-      } else {
-        document.querySelector("#loader").style.display = "none";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
 
-        document.querySelector("body").style.visibility = "visible";
-      }
-    };
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
 
-    function openMenu() {
-      document.querySelector("#brandName").style.visibility = "hidden";
-      document.querySelector("#nav-barx").style.display = "block";
-      document.querySelector("#menu").style.left = "0%";
-      document.querySelector("#nav-bar").style.display = "none";
-    }
-    function closeMenu() {
-      document.querySelector("#menu").style.left = "100%";
-      document.querySelector("#brandName").style.visibility = "visible";
-      document.querySelector("#nav-barx").style.display = "none";
-      document.querySelector("#nav-bar").style.display = "block";
-    }
+function openMenu() {
+  document.querySelector("#brandName").style.visibility = "hidden";
+  document.querySelector("#nav-barx").style.display = "block";
+  document.querySelector("#menu").style.left = "0%";
+  document.querySelector("#nav-bar").style.display = "none";
+}
+function closeMenu() {
+  document.querySelector("#menu").style.left = "100%";
+  document.querySelector("#brandName").style.visibility = "visible";
+  document.querySelector("#nav-barx").style.display = "none";
+  document.querySelector("#nav-bar").style.display = "block";
+}
 
-const imageSourcesArray = [
-  "./img/showcaseimages/desk1.png",
-  "./img/showcaseimages/desk2.png",
-  "./img/showcaseimages/desk3.png",
-  "./img/showcaseimages/desk4.png",
-  "./img/showcaseimages/desk5mob1.png",
-  "./img/showcaseimages/desk6.png",
-  "./img/showcaseimages/desk7mob2.png",
-  "./img/showcaseimages/desk8mob3.png",
-  "./img/showcaseimages/desk9mob4.png",
-  "./img/showcaseimages/desk10.png",
-  "./img/showcaseimages/desk11mob5.png",
-  "./img/showcaseimages/desk12mob6.png",
-  "./img/showcaseimages/desk13.png",
-  "./img/showcaseimages/desk14mob7.png",
-  "./img/showcaseimages/desk15mob8.png",
-  "./img/showcaseimages/desk16mob9.png",
-  "./img/showcaseimages/desk17.png",
-  "./img/showcaseimages/desk18mob10.png",
-  "./img/showcaseimages/desk19.png",
-  "./img/showcaseimages/desk20.png",
-  "./img/showcaseimages/desk21.png",
-  "./img/showcaseimages/hero22.png",
-  "./img/showcaseimages/hero23.png",
-  "./img/showcaseimages/hero24.png",
-  "./img/showcaseimages/hero25.png",
-  "./img/showcaseimages/hero26.png",
-  "./img/showcaseimages/hero27.png",
-  "./img/showcaseimages/hero28.png",
-  "./img/showcaseimages/hero29.png",
-  "./img/showcaseimages/hero30.png",
-  "./img/showcaseimages/hero31.png",
-  "./img/showcaseimages/hero32.png",
-  "./img/showcaseimages/hero33.png",
-];
-
-// Create an array of image sources
-const imageSources = Array.from(
-  document.querySelectorAll("img.showcaseimg")
-).map((img) => img.getAttribute("src"));
-const images = Array.from(document.querySelectorAll(".showcaseimg"));
-// // Keep track of which images have been shown
-const usedImages = [];
-
-function getRandomImageSrc() {
-  let imageSrc =
-    imageSourcesArray[Math.floor(Math.random() * imageSourcesArray.length)];
-  while (usedImages.includes(imageSrc)) {
-    imageSrc =
+if (window.location.pathname === "/") {
+  const imageSourcesArray = [
+    "./img/showcaseimages/desk1.png",
+    "./img/showcaseimages/desk2.png",
+    "./img/showcaseimages/desk3.png",
+    "./img/showcaseimages/desk4.png",
+    "./img/showcaseimages/desk5mob1.png",
+    "./img/showcaseimages/desk6.png",
+    "./img/showcaseimages/desk7mob2.png",
+    "./img/showcaseimages/desk8mob3.png",
+    "./img/showcaseimages/desk9mob4.png",
+    "./img/showcaseimages/desk10.png",
+    "./img/showcaseimages/desk11mob5.png",
+    "./img/showcaseimages/desk12mob6.png",
+    "./img/showcaseimages/desk13.png",
+    "./img/showcaseimages/desk14mob7.png",
+    "./img/showcaseimages/desk15mob8.png",
+    "./img/showcaseimages/desk16mob9.png",
+    "./img/showcaseimages/desk17.png",
+    "./img/showcaseimages/desk18mob10.png",
+    "./img/showcaseimages/desk19.png",
+    "./img/showcaseimages/desk20.png",
+    "./img/showcaseimages/desk21.png",
+    "./img/showcaseimages/hero22.png",
+    "./img/showcaseimages/hero23.png",
+    "./img/showcaseimages/hero24.png",
+    "./img/showcaseimages/hero25.png",
+    "./img/showcaseimages/hero26.png",
+    "./img/showcaseimages/hero27.png",
+    "./img/showcaseimages/hero28.png",
+    "./img/showcaseimages/hero29.png",
+    "./img/showcaseimages/hero30.png",
+    "./img/showcaseimages/hero31.png",
+    "./img/showcaseimages/hero32.png",
+    "./img/showcaseimages/hero33.png",
+  ];
+  
+  // Create an array of image sources
+  const imageSources = Array.from(
+    document.querySelectorAll("img.showcaseimg")
+  ).map((img) => img.getAttribute("src"));
+  const images = Array.from(document.querySelectorAll(".showcaseimg"));
+  // // Keep track of which images have been shown
+  const usedImages = [];
+  
+  function getRandomImageSrc() {
+    let imageSrc =
       imageSourcesArray[Math.floor(Math.random() * imageSourcesArray.length)];
+    while (usedImages.includes(imageSrc)) {
+      imageSrc =
+        imageSourcesArray[Math.floor(Math.random() * imageSourcesArray.length)];
+    }
+    usedImages.push(imageSrc);
+    return imageSrc;
   }
-  usedImages.push(imageSrc);
-  return imageSrc;
-}
-
-function showRandomImage() {
-  if (usedImages.length === images.length) {
-    // Reset the usedImages array
-    usedImages.length = 0;
+  
+  function showRandomImage() {
+    if (usedImages.length === images.length) {
+      // Reset the usedImages array
+      usedImages.length = 0;
+    }
+    const randomIndex = Math.floor(Math.random() * images.length);
+    // choose a random image
+  
+    const randomImage = images[randomIndex];
+    addAnimation(randomImage);
+  
+    const imageSrc = getRandomImageSrc();
+    randomImage.setAttribute("src", imageSrc);
   }
-  const randomIndex = Math.floor(Math.random() * images.length);
-  // choose a random image
-
-  const randomImage = images[randomIndex];
-  addAnimation(randomImage);
-
-  const imageSrc = getRandomImageSrc();
-  randomImage.setAttribute("src", imageSrc);
+  setInterval(showRandomImage, 1000);
+  function addAnimation(element) {
+    setTimeout(() => {
+      element.classList.add(
+        "animate__animated",
+        "animate__fadeIn",
+        "animate__delay-10s"
+      );
+    }, 900);
+  
+    setTimeout(() => {
+      element.classList.remove(
+        "animate__animated",
+        "animate__fadeIn",
+        "animate__delay-1s"
+      );
+    }, 3000);
+  }
 }
 
-setInterval(showRandomImage, 1000);
-
-function addAnimation(element) {
-  setTimeout(() => {
-    element.classList.add(
-      "animate__animated",
-      "animate__fadeIn",
-      "animate__delay-10s"
-    );
-  }, 900);
-
-  setTimeout(() => {
-    element.classList.remove(
-      "animate__animated",
-      "animate__fadeIn",
-      "animate__delay-1s"
-    );
-  }, 3000);
-}
 
 function firstPlus() {
   document.getElementsByClassName("secondSectionSecondDiv")[0].style.display =
